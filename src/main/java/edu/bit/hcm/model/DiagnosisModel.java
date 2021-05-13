10bc54db092bad7e7561dcc43793a2e5eaf2a0c6
Harsha.Hulangamuwa <harshanuwanh@gmail.com>
2021-05-13 23:06:58 +0530

    New-Changes-13-05-2021 - commiting new changes

--- src/main/java/edu/bit/hcm/model/DiagnosisModel.java
+++ src/main/java/edu/bit/hcm/model/DiagnosisModel.java
@@ -1,18 +1,26 @@
 package edu.bit.hcm.model;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.bit.hcm.entity.DiagnosisEntity;
+import edu.bit.hcm.entity.PatientEntity;
 import edu.bit.hcm.repository.DiagnosisRepository;
+import edu.bit.hcm.repository.PatientRepository;
 
 @Service
 public class DiagnosisModel {
 
 	@Autowired
 	private DiagnosisRepository diagnosisRepository;
+
+	@Autowired
+	private PatientRepository patientRepository;
 
 	public void saveDiagnosis(DiagnosisEntity diagnosisEntity) {
 		diagnosisRepository.save(diagnosisEntity);
@@ -22,9 +30,27 @@ public class DiagnosisModel {
 	public List<DiagnosisEntity> findByPidAndDoctorId(int pid, int doctorId) {
 		return diagnosisRepository.findByPidAndDoctorID(pid, doctorId);
 	}
-	
+
 	public void updatePrescriptionStatus(int dId, boolean status) {
 		diagnosisRepository.updatePrescriptionStatus(new Boolean(status), dId);
+	}
+
+	public List<DiagnosisEntity> findByDate(String date) {
+		Date dd = null;
+		try {
+			dd = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(date).getTime());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return diagnosisRepository.findByDate(dd);
+	}
+
+	public PatientEntity getPatienEntityById(int id) {
+		return patientRepository.findByPatientId(id);
+	}
+
+	public void completeLabReport(int diagnosisId) {
+		diagnosisRepository.updateLabReportStatus(true, diagnosisId);
 	}
 
 }
--- src/main/java/edu/bit/hcm/repository/DiagnosisRepository.java
+++ src/main/java/edu/bit/hcm/repository/DiagnosisRepository.java
@@ -1,5 +1,6 @@
 package edu.bit.hcm.repository;
 
+import java.sql.Date;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -17,9 +18,17 @@ public interface DiagnosisRepository extends JpaRepository<DiagnosisEntity, Inte
 	@Query("SELECT d FROM DiagnosisEntity d WHERE d.pid = ?1 AND d.doctorId = ?2")
 	public List<DiagnosisEntity> findByPidAndDoctorID(Integer pid, Integer doctorId);
 	
+	@Query("SELECT d FROM DiagnosisEntity d WHERE d.date = ?1")
+	public List<DiagnosisEntity> findByDate(Date date);
+	
 	@Transactional
 	@Modifying
 	@Query("UPDATE DiagnosisEntity d SET d.prescriptionStatus=?1 WHERE d.diagnosisId = ?2")
 	public void updatePrescriptionStatus(Boolean status, Integer diagnosis_id);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE DiagnosisEntity d SET d.reportStatus=?1 WHERE d.diagnosisId = ?2")
+	public void updateLabReportStatus(Boolean status, Integer diagnosis_id);
 
 }
